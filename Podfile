a85a58b889fa026cc5101cb76d1211106d204c1a
Burcu Geneci <burcu.geneci@gmail.com>
2018-06-23 13:24:18 +0300

    initial project commit.

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ProjectX' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ProjectX
+
+  target 'ProjectXTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ProjectXUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ProjectX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectX
+//
+//  Created by Burcu Geneci on 23.06.2018.
+//  Copyright © 2018 GDG Istanbul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
