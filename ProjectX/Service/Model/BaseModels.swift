140bdb81ef87e4b3d0fae34dec0888071ef33e07
Burcu Geneci <burcu.geneci@gmail.com>
2018-08-02 20:40:14 +0300

    BaseModels added.

---
+++ ProjectX/Service/Model/BaseModels.swift
@@ -0,0 +1,16 @@
+//
+//  BaseModel.swift
+//  ProjectX
+//
+//  Created by Burcu Geneci on 2.08.2018.
+//  Copyright © 2018 GDG Istanbul. All rights reserved.
+//
+
+import UIKit
+import EVReflection
+
+class BaseModel: EVObject { }
+
+class BaseRequestModel : BaseModel { }
+
+class BaseResponseModel : BaseModel { }
